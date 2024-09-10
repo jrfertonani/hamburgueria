db05a20744377d1dbf5dfee0317415db791cfaa7
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-09-10 11:24:37 -0300

    bebidas - CRUD

--- backend/src/main/java/hemburgueria/domain/entity/DTO/bebidaDTO.java
+++ backend/src/main/java/hemburgueria/domain/entity/DTO/bebidaDTO.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter     @Setter
-public class bebidasDTO implements Serializable {
+public class bebidaDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer id;
