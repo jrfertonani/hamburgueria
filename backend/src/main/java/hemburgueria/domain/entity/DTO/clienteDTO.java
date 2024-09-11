dfbd61a4669090fe87f11ceed50f430e4c42f7ed
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-09-11 09:43:52 -0300

    domain - dto - repository -

---
+++ backend/src/main/java/hemburgueria/domain/entity/DTO/clienteDTO.java
@@ -0,0 +1,24 @@
+package hemburgueria.domain.entity.DTO;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter     @Setter
+public class clienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+
+}
