9a6b48fa9f9348a89df035ce4c2401d5d3c1c8bf
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-05 14:47:11 -0300

    Pedidos Create

--- front/src/app/components/views/pedido/pedido-create/pedido-create.component.ts
+++ front/src/app/components/views/pedido/pedido-create/pedido-create.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PedidoService } from '../pedido.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pedido } from '../pedido.module';
 
 @Component({
   selector: 'app-pedido-create',
   templateUrl: './pedido-create.component.html',
   styleUrl: './pedido-create.component.scss'
 })
-export class PedidoCreateComponent {
+export class PedidoCreateComponent implements OnInit {
+
+  constructor(
+    private pedidosService: PedidoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  pedido: Pedido[] =[];
+
+
+  ngOnInit(): void {
+
+  }
+
+
+  save(){}
+
+
+  cancel(){
+    this.router.navigate(['/pedidos'])
+  }
+
+
 
 }
