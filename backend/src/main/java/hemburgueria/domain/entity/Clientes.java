16f0c95cd82795b2bc4b74b68eb1fbe2d92a9495
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-04 21:00:40 -0300

    cliente Entity

---
+++ backend/src/main/java/hemburgueria/domain/entity/Clientes.java
@@ -0,0 +1,20 @@
+package hemburgueria.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class Clientes extends Pessoas implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "clientes")
+    private List<Pedidos> pedidos = new ArrayList<>();
+
+
+}
+
