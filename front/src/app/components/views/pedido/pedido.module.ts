98e06d8dbc6e95e8e410ea2ccb73bc08453127a5
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-05 17:58:15 -0300

    pedidos module ts

--- front/src/app/components/views/pedido/pedido.module.ts
+++ front/src/app/components/views/pedido/pedido.module.ts
@@ -4,7 +4,7 @@ import { Lanche } from "../lanche/lanche.module";
 
 export interface Pedido {
   id?: number;
-  lanche: Lanche[];
-  bebida: Bebida;
-  cliente: Cliente;
+  lanche: string;
+  bebida: string;
+  cliente: string;
 }
