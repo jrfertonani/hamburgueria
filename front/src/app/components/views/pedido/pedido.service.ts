9a415d938191acbfbc458eb6d9a2e3d12353bea0
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-04 21:54:44 -0300

    pedidos service ts

---
+++ front/src/app/components/views/pedido/pedido.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Pedido } from './pedido.module';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidoService {
+
+  http = inject(HttpClient);
+
+  URL = 'http://localhost:8080/pedidos';
+
+
+  constructor(private _snack: MatSnackBar) {}
+
+  pedido: [] = [];
+
+  findAll():Observable<Pedido[]>{
+    return this.http.get<Pedido[]>(this.URL);
+  }
+
+}
